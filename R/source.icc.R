942d875212e2b74f4794cc231f2206c677faecfe
devillemereuil <bonamy@horus.ens.fr>
2016-09-26 15:22:07 +1300

    Adding closed forms for Poisson and Gaussian

--- R/source.icc.R
+++ R/source.icc.R
@@ -18,46 +18,82 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+##-----------------------------Special functions for known analytical solutions--------------
+
+qg.Gaussian.icc=function(mu=NULL,var.comp,var.p,predict=NULL) {
+  if(length(mu)>1 | length(var.comp)!=1 | length(var.p) != 1) stop("The parameters mu, var.comp and var.p must be of length 1, please check your input.")
+  if (is.null(predict)) { if(is.null(mu)) {stop("Please provide either mu or predict.")} else {predict=mu;}}
+  #Nothing to be done, except averaging over predict
+  if (length(predict)==1) {var_fixed=0} else {var_fixed=var(predict)}
+  data.frame(mean.obs=mean(predict),var.obs=var.p+var_fixed,var.comp.obs=var.comp,icc.obs=var.comp/(var.p+var_fixed))
+}
+
+qg.Poisson.log.icc <- function(mu=NULL,var.comp,var.p,predict=NULL){
+  if(length(mu)>1 | length(var.comp)!=1 | length(var.p) != 1) stop("The parameters mu, var.comp and var.p must be of length 1, please check your input.")
+  if (is.null(predict)) { if(is.null(mu)) {stop("Please provide either mu or predict.")} else {predict=mu;}}
+  #Observed mean
+  lambda = mean(exp(predict+(var.p/2)))
+  #Mean of lambda square, needed for the following
+  lambda_sq = mean(exp(2*(predict+var.p/2)))
+  #Observed variance
+  var_obs = lambda_sq*exp(var.p)-lambda**2+lambda
+  #Component variance
+  var_comp_obs = exp(var.comp + var.p)*(mean(exp(predict))^2) - (lambda^2)
+  data.frame(mean.obs=lambda,var.obs=var_obs,var.comp.obs=var_comp_obs,icc.obs=var_comp_obs/var_obs)
+}
+
+##-------------------------------General functions--------------------------------------------
+
 QGicc <- function(mu=NULL, var.comp, var.p, model="", width=10, predict=NULL, closed.form=TRUE, custom.model=NULL, n.obs=NULL, cut.points=NULL, theta=NULL, verbose=TRUE){
   if(length(mu)>1 | length(var) >1) stop("The parameters mu and var must be of length 1, please check your input.")
   if (is.null(predict)) { if(is.null(mu)) {stop("Please provide either mu or predict.")} else {predict=mu;}}
   
-  #Use a custom model if defined, otherwise look into the "Dictionary"
-  if (is.null(custom.model)) {
-    if (model==""){stop("The function requires either model or custom.model.")} else {
-      funcs=QGlink.funcs(model,n.obs=n.obs,theta=theta)
-    }} else {funcs=custom.model}
-
-  #Observed mean computation
-  if (verbose) print("Computing observed mean...")
-  z_bar=QGmean(mu=mu,var=var.p,link.inv=funcs$inv.link,width=width,predict=predict)
-
-  #Phenotypic variances computation
-  if (verbose) print("Computing phenotypic variance...")
-  var_exp=QGvar.exp(mu=mu,var=var.p,link.inv=funcs$inv.link,obs.mean=z_bar,width=width,predict=predict)
-  var_dist=QGvar.dist(mu=mu,var=var.p,var.func=funcs$var.func,width=width,predict=predict)
-  var_obs=var_exp+var_dist
-   
-  #Computing conditional variance (i.e. without var.comp)
-  var.cond <- var.p - var.comp
+  #Using analytical solutions if possible (and asked for, see closed.form arg)
+  if (model=="Gaussian"&closed.form) {
+    if (verbose) print("Using the closed forms for a Gaussian model with identity link (e.g. LMM).")
+    qg.Gaussian.icc(mu=mu,var.comp=var.comp,var.p=var.p,predict=predict)
+  } else if (model=="Poisson.log"&closed.form) {
+    if(verbose) print("Using the closed forms for a Poisson-log model.")
+    qg.Poisson.log.icc(mu=mu,var.comp=var.comp,var.p=var.p,predict=predict)
+  } else {
   
-  #Function giving the conditional expectancy
-  cond_exp <- function(t){
-    sapply(t,function(t_i){
-      mean(sapply(predict,function(pred_i){
-	integrate(f=function(u){funcs$inv.link(u)*dnorm(u,pred_i+t_i,sqrt(var.cond))},
-	lower=pred_i+t_i-width*sqrt(var.cond),upper=pred_i+t_i+width*sqrt(var.cond))$value
-      }))
-    })
+    #Use a custom model if defined, otherwise look into the "Dictionary"
+    if (is.null(custom.model)) {
+      if (model==""){stop("The function requires either model or custom.model.")} else {
+	funcs=QGlink.funcs(model,n.obs=n.obs,theta=theta)
+      }} else {funcs=custom.model}
+
+    #Observed mean computation
+    if (verbose) print("Computing observed mean...")
+    z_bar=QGmean(mu=mu,var=var.p,link.inv=funcs$inv.link,width=width,predict=predict)
+
+    #Phenotypic variances computation
+    if (verbose) print("Computing phenotypic variance...")
+    var_exp=QGvar.exp(mu=mu,var=var.p,link.inv=funcs$inv.link,obs.mean=z_bar,width=width,predict=predict)
+    var_dist=QGvar.dist(mu=mu,var=var.p,var.func=funcs$var.func,width=width,predict=predict)
+    var_obs=var_exp+var_dist
+    
+    #Computing conditional variance (i.e. without var.comp)
+    var.cond <- var.p - var.comp
+    
+    #Function giving the conditional expectancy
+    cond_exp <- function(t){
+      sapply(t,function(t_i){
+	mean(sapply(predict,function(pred_i){
+	  integrate(f=function(u){funcs$inv.link(u)*dnorm(u,pred_i+t_i,sqrt(var.cond))},
+	  lower=pred_i+t_i-width*sqrt(var.cond),upper=pred_i+t_i+width*sqrt(var.cond))$value
+	}))
+      })
+    }
+    
+    #Computing variance of component on the observed data scale
+    #Note: Using Koenig's formula
+    if (verbose) print("Computing component variance...")
+    var_comp_obs <- integrate(f=function(x){((cond_exp(x)-z_bar)^2)*dnorm(x,0,sqrt(var.comp))},lower=-width*sqrt(var.comp),upper=width*sqrt(var.comp))$value
+    
+    #Return a data.frame with the calculated components
+    data.frame(mean.obs=z_bar,var.obs=var_obs,var.comp.obs=var_comp_obs,icc.obs=var_comp_obs/var_obs)
   }
-  
-  #Computing variance of component on the observed data scale
-  #Note: Using Koenig's formula
-  if (verbose) print("Computing component variance...")
-  var_comp_obs <- integrate(f=function(x){((cond_exp(x)-z_bar)^2)*dnorm(x,0,sqrt(var.comp))},lower=-width*sqrt(var.comp),upper=width*sqrt(var.comp))$value
-  
-  #Return a data.frame with the calculated components
-  data.frame(mean.obs=z_bar,var.obs=var_obs,var.comp.obs=var_comp_obs,icc.obs=var_comp_obs/var_obs)
 }
 
 
